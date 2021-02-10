ef5ec157117a3a517e528506c95b9f031db8610c
stu.chen <stuchen_cn@163.com>
2021-02-10 09:08:20 +0800

    feature: package version

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,7 +5,16 @@ import 'lib-flexible'
 import Header from './components/Header'
 import iButton from './components/iButton'
 import iDialog from './components/iDialog'
+import packageInfo from '../package.json'
 const app = createApp(App)
+
+console.log(`App version: ${packageInfo.version}`)
+console.log(`Vue version: ${app.version}`)
+
 app.config.globalProperties.$dialog = iDialog
-app.use(Header).use(iButton)
-app.use(router).mount('#app')
+
+app
+  .use(Header)
+  .use(iButton)
+  .use(router)
+  .mount('#app')
